491523d1565a8a2600d07421fc9c8cebb092d5f9
anduuin <jcm2232@gmail.com>
2022-08-03 18:46:59 +0900

    style: pong 스타일 수정

--- backend/app/src/pong/match.service.ts
+++ backend/app/src/pong/match.service.ts
@@ -1,42 +1,53 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable } from '@nestjs/common'
 import { Socket } from 'socket.io'
 
 type MatchData = {
-  uid: number,
-  socket: Socket,
+  uid: number
+  socket: Socket
 }
 
 @Injectable()
 export class MatchService {
-  
-  private readonly quickQueue: MatchData[] = [];
-  private readonly rankedQueue: MatchData[] = [];
-  
-  matchRanked(player: MatchData): {left: MatchData, right: MatchData} | null {
+  private readonly quickQueue: MatchData[] = []
+  private readonly rankedQueue: MatchData[] = []
+
+  matchRanked(player: MatchData): { left: MatchData; right: MatchData } | null {
     this.rankedQueue.push(player)
     if (this.rankedQueue.length >= 2) {
       const left = this.rankedQueue.shift()
       const right = this.rankedQueue.shift()
-      return {left, right}
+      return { left, right }
     }
     return null
   }
 
-  matchQuick(player: MatchData): {left: MatchData, right: MatchData} | null {
+  matchQuick(player: MatchData): { left: MatchData; right: MatchData } | null {
     this.quickQueue.push(player)
-    console.log(this.quickQueue.map(elem => elem.uid))
+    console.log(this.quickQueue.map((elem) => elem.uid))
     if (this.quickQueue.length >= 2) {
       const left = this.quickQueue.shift()
       const right = this.quickQueue.shift()
-      return {left, right}
+      return { left, right }
     }
     return null
   }
 
   removeFromQueue(player: Socket) {
-    console.log('before remove', this.quickQueue.map(elem => elem.uid))
-    this.quickQueue.splice(this.quickQueue.findIndex(p => p.socket.id === player.id), 1)
-    this.rankedQueue.splice(this.rankedQueue.findIndex(p => p.socket.id === player.id), 1)
-    console.log('after remove', this.quickQueue.map(elem => elem.uid))
+    console.log(
+      'before remove',
+      this.quickQueue.map((elem) => elem.uid),
+    )
+    this.quickQueue.splice(
+      this.quickQueue.findIndex((p) => p.socket.id === player.id),
+      1,
+    )
+    this.rankedQueue.splice(
+      this.rankedQueue.findIndex((p) => p.socket.id === player.id),
+      1,
+    )
+    console.log(
+      'after remove',
+      this.quickQueue.map((elem) => elem.uid),
+    )
   }
 }
--- backend/app/src/pong/pong.service.ts
+++ backend/app/src/pong/pong.service.ts
@@ -1,11 +1,11 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable } from '@nestjs/common'
 import { Socket } from 'socket.io'
 
 import * as CONSTANTS from './constants'
 
 type MatchData = {
-  uid: number,
-  socket: Socket,
+  uid: number
+  socket: Socket
 }
 
 class Rect {
@@ -35,7 +35,14 @@ class MoveableRect extends Rect {
   speedX: number
   speedY: number
 
-  constructor(x: number, y: number, width: number, height: number, speedX: number, speedY: number) {
+  constructor(
+    x: number,
+    y: number,
+    width: number,
+    height: number,
+    speedX: number,
+    speedY: number,
+  ) {
     super(x, y, width, height)
     this.speedX = speedX
     this.speedY = speedY
@@ -45,22 +52,23 @@ class MoveableRect extends Rect {
 class Paddle extends MoveableRect {
   constructor(isLeft: boolean) {
     const paddleXOffset = isLeft
-                          ? CONSTANTS.PADDLE_WIDTH
-                          : CONSTANTS.WINDOW_WIDTH - 2 * CONSTANTS.PADDLE_WIDTH
-    const paddleYOffset = CONSTANTS.WINDOW_HEIGHT / 2 - CONSTANTS.PADDLE_HEIGHT / 2
-    super (
+      ? CONSTANTS.PADDLE_WIDTH
+      : CONSTANTS.WINDOW_WIDTH - 2 * CONSTANTS.PADDLE_WIDTH
+    const paddleYOffset =
+      CONSTANTS.WINDOW_HEIGHT / 2 - CONSTANTS.PADDLE_HEIGHT / 2
+    super(
       paddleXOffset,
       paddleYOffset,
       CONSTANTS.PADDLE_WIDTH,
       CONSTANTS.PADDLE_HEIGHT,
       0,
-      0
+      0,
     )
   }
 }
 
 function toRad(deg: number) {
-  return deg / 180 * Math.PI
+  return (deg / 180) * Math.PI
 }
 
 function reflect(dot: number, mirror: number) {
@@ -70,9 +78,9 @@ function reflect(dot: number, mirror: number) {
 class Ball extends MoveableRect {
   constructor(difficulty: CONSTANTS.PongMode) {
     let speedFactor: number
-    switch(difficulty) {
+    switch (difficulty) {
       case 'easy':
-        speedFactor = CONSTANTS.EASY_SPEED  
+        speedFactor = CONSTANTS.EASY_SPEED
         break
       case 'medium':
         speedFactor = CONSTANTS.MEDIUM_SPEED
@@ -81,36 +89,36 @@ class Ball extends MoveableRect {
         speedFactor = CONSTANTS.HARD_SPEED
         break
     }
-    
-    const theta = toRad(Math.random()
-                * CONSTANTS.MAX_SHOOT_RIGHT_UP_DEGREE * 2
-                - CONSTANTS.MAX_SHOOT_RIGHT_UP_DEGREE)
+
+    const theta = toRad(
+      Math.random() * CONSTANTS.MAX_SHOOT_RIGHT_UP_DEGREE * 2 -
+        CONSTANTS.MAX_SHOOT_RIGHT_UP_DEGREE,
+    )
 
     let speedX = Math.cos(theta) * speedFactor
     const speedY = Math.sin(theta) * speedFactor
 
-    if (Math.random() < 0.5)
-      speedX = -speedX
-    
-    super (
+    if (Math.random() < 0.5) speedX = -speedX
+
+    super(
       CONSTANTS.WINDOW_HEIGHT / 2 - CONSTANTS.BALL_SIZE / 2,
       CONSTANTS.WINDOW_WIDTH / 2 - CONSTANTS.BALL_SIZE / 2,
       CONSTANTS.BALL_SIZE,
       CONSTANTS.BALL_SIZE,
       speedX,
-      speedY
+      speedY,
     )
   }
 }
 
 class Pong {
-  private ball: Ball;
-  private leftPaddle: Paddle;
-  private rightPaddle: Paddle;
+  private ball: Ball
+  private leftPaddle: Paddle
+  private rightPaddle: Paddle
 
-  private leftScore: number = 0
-  private rightScore: number = 0
-  private lastUpdate: number = 0
+  private leftScore = 0
+  private rightScore = 0
+  private lastUpdate = 0
   private difficulty: CONSTANTS.PongMode
   private winner: 'left' | 'right' | null = null
 
@@ -149,8 +157,10 @@ class Pong {
     return this.winner
   }
 
-  changePaddleVelocity(target: 'left' | 'right', direction: 'up' | 'down' | 'stop') {
-    
+  changePaddleVelocity(
+    target: 'left' | 'right',
+    direction: 'up' | 'down' | 'stop',
+  ) {
     let velocity: number
     switch (direction) {
       case 'up':
@@ -172,10 +182,10 @@ class Pong {
 
   updateBall(timestamp: number) {
     if (this.winner) {
-      return;
+      return
     }
-    this.ball.x += this.ball.speedX * (timestamp - this.lastUpdate) / 1000
-    this.ball.y += this.ball.speedY * (timestamp - this.lastUpdate) / 1000
+    this.ball.x += (this.ball.speedX * (timestamp - this.lastUpdate)) / 1000
+    this.ball.y += (this.ball.speedY * (timestamp - this.lastUpdate)) / 1000
 
     // 상단 충돌 처리
     if (this.ball.y < 0) {
@@ -185,19 +195,28 @@ class Pong {
 
     // 하단 충돌 처리
     else if (this.ball.y > CONSTANTS.WINDOW_HEIGHT - CONSTANTS.BALL_SIZE) {
-      this.ball.y = reflect(this.ball.y, CONSTANTS.WINDOW_HEIGHT - CONSTANTS.BALL_SIZE)
+      this.ball.y = reflect(
+        this.ball.y,
+        CONSTANTS.WINDOW_HEIGHT - CONSTANTS.BALL_SIZE,
+      )
       this.ball.speedY = -this.ball.speedY
     }
 
     // 왼쪽 패들 충돌 처리
     else if (this.leftPaddle.isCollision(this.ball)) {
-      this.ball.x = reflect(this.ball.x, this.leftPaddle.x + this.leftPaddle.width)
+      this.ball.x = reflect(
+        this.ball.x,
+        this.leftPaddle.x + this.leftPaddle.width,
+      )
       this.ball.speedX = -this.ball.speedX
     }
 
     // 오른쪽 패들 충돌 처리
     else if (this.rightPaddle.isCollision(this.ball)) {
-      this.ball.x = reflect(this.ball.x, this.rightPaddle.x - this.rightPaddle.width)
+      this.ball.x = reflect(
+        this.ball.x,
+        this.rightPaddle.x - this.rightPaddle.width,
+      )
       this.ball.speedX = -this.ball.speedX
     }
 
@@ -215,26 +234,31 @@ class Pong {
   }
 
   updatePaddle(timestamp: number) {
-    if (this.winner)
-      return
-    this.leftPaddle.y += this.leftPaddle.speedY * (timestamp - this.lastUpdate) / 1000
-    this.rightPaddle.y += this.rightPaddle.speedY * (timestamp - this.lastUpdate) / 1000
+    if (this.winner) return
+    this.leftPaddle.y +=
+      (this.leftPaddle.speedY * (timestamp - this.lastUpdate)) / 1000
+    this.rightPaddle.y +=
+      (this.rightPaddle.speedY * (timestamp - this.lastUpdate)) / 1000
 
-    if (this.leftPaddle.y < 0)
-      this.leftPaddle.y = 0
-    else if (this.leftPaddle.y > CONSTANTS.WINDOW_HEIGHT - CONSTANTS.PADDLE_HEIGHT)
+    if (this.leftPaddle.y < 0) this.leftPaddle.y = 0
+    else if (
+      this.leftPaddle.y >
+      CONSTANTS.WINDOW_HEIGHT - CONSTANTS.PADDLE_HEIGHT
+    )
       this.leftPaddle.y = CONSTANTS.WINDOW_HEIGHT - CONSTANTS.PADDLE_HEIGHT
-    
-    if (this.rightPaddle.y < 0)
-      this.rightPaddle.y = 0
-    else if (this.rightPaddle.y > CONSTANTS.WINDOW_HEIGHT - CONSTANTS.PADDLE_HEIGHT)
+
+    if (this.rightPaddle.y < 0) this.rightPaddle.y = 0
+    else if (
+      this.rightPaddle.y >
+      CONSTANTS.WINDOW_HEIGHT - CONSTANTS.PADDLE_HEIGHT
+    )
       this.rightPaddle.y = CONSTANTS.WINDOW_HEIGHT - CONSTANTS.PADDLE_HEIGHT
   }
 
   // FIXME: 타임스탬프가 매우 커서 한번 업데이트에 두번 이상 벽에 충돌하는 경우 처리해야 함
   update(timestamp: number) {
     if (this.winner) {
-      return;
+      return
     }
     this.updateBall(timestamp)
     this.updatePaddle(timestamp)
@@ -243,22 +267,22 @@ class Pong {
 }
 
 type RunningPongGame = {
-  game: Pong;
-  leftTimer: NodeJS.Timer;
-  rightTimer: NodeJS.Timer;
-  gameTimer: NodeJS.Timer;
+  game: Pong
+  leftTimer: NodeJS.Timer
+  rightTimer: NodeJS.Timer
+  gameTimer: NodeJS.Timer
 }
 
 type PongUser = {
-  game: Pong;
-  isLeftSide: boolean;
+  game: Pong
+  isLeftSide: boolean
 }
 
 @Injectable()
 export class PongService {
   private nextId = 0
-  private readonly games: Map<number, RunningPongGame> = new Map();
-  private readonly gamesByUser: Map<string, PongUser> = new Map();
+  private readonly games: Map<number, RunningPongGame> = new Map()
+  private readonly gamesByUser: Map<string, PongUser> = new Map()
 
   abortGame(game: RunningPongGame) {
     clearInterval(game.leftTimer)
@@ -266,45 +290,57 @@ export class PongService {
     clearInterval(game.gameTimer)
   }
 
-  updateGame(game: RunningPongGame, user: {left: MatchData, right: MatchData}) {
+  updateGame(
+    game: RunningPongGame,
+    user: { left: MatchData; right: MatchData },
+  ) {
     game.game.update(Date.now())
     const winner = game.game.getWinner()
     if (winner) {
       this.abortGame(game)
       user.left.socket.emit('render', game.game)
       user.right.socket.emit('render', game.game)
-      user.left.socket.emit('gameEnd', {winner})
-      user.right.socket.emit('gameEnd', {winner})
+      user.left.socket.emit('gameEnd', { winner })
+      user.right.socket.emit('gameEnd', { winner })
       user.left.socket.disconnect()
       user.right.socket.disconnect()
     }
   }
 
-  startGame(difficulty: CONSTANTS.PongMode, user: {left: MatchData, right: MatchData}): number {
+  startGame(
+    difficulty: CONSTANTS.PongMode,
+    user: { left: MatchData; right: MatchData },
+  ): number {
     const game = new Pong(difficulty)
-    
+
     const gameId = this.nextId
 
     const runningGame = {
       game,
-      leftTimer: setInterval(() => { user.left.socket.emit('render', game) }, CONSTANTS.UPDATE_INTERVAL),
-      rightTimer: setInterval(() => { user.right.socket.emit('render', game) }, CONSTANTS.UPDATE_INTERVAL),
-      gameTimer: setInterval(() => { this.updateGame(runningGame, user) }, CONSTANTS.UPDATE_INTERVAL)
+      leftTimer: setInterval(() => {
+        user.left.socket.emit('render', game)
+      }, CONSTANTS.UPDATE_INTERVAL),
+      rightTimer: setInterval(() => {
+        user.right.socket.emit('render', game)
+      }, CONSTANTS.UPDATE_INTERVAL),
+      gameTimer: setInterval(() => {
+        this.updateGame(runningGame, user)
+      }, CONSTANTS.UPDATE_INTERVAL),
     }
     this.games.set(gameId, runningGame)
-  
-    this.gamesByUser.set(user.left.socket.id , {game, isLeftSide: true})
-    this.gamesByUser.set(user.right.socket.id , {game, isLeftSide: false})
-  
+
+    this.gamesByUser.set(user.left.socket.id, { game, isLeftSide: true })
+    this.gamesByUser.set(user.right.socket.id, { game, isLeftSide: false })
+
     this.nextId++
     return gameId
   }
 
   getGameByGameId(gameId: number): RunningPongGame | null {
     return this.games.get(gameId)
-  } 
+  }
 
   getGameBySocketId(socketId: string): PongUser | null {
     return this.gamesByUser.get(socketId)
-  } 
+  }
 }
