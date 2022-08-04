d3bb781db761f27c776c1343af9cf313569bb09a
cjeon <cjeon@student.42seoul.kr>
2022-08-04 17:36:42 +0900

    feat: 소켓 연결에서 jwt를 확인하는 로직 구현

--- backend/app/src/pong/match.gateway.ts
+++ backend/app/src/pong/match.gateway.ts
@@ -4,14 +4,16 @@ import {
   SubscribeMessage,
   WebSocketGateway,
   OnGatewayDisconnect,
+  OnGatewayConnection,
 } from '@nestjs/websockets'
 import { Socket } from 'socket.io'
 import { PongMatchType, PongMode } from './constants'
 import { MatchService } from './match.service'
 import { PongService } from './pong.service'
+import * as jwt from 'jsonwebtoken'
+import { jwtConstants } from 'src/configs/jwttoken.config'
 
 export type MatchMessage = {
-  uid: number
   mode: PongMode
   matchType: PongMatchType
 }
@@ -26,8 +28,12 @@ type PongKeyEvent = {
   isDown: boolean
 }
 
+interface UserSocket extends Socket {
+  uid: number
+}
+
 @WebSocketGateway({ namespace: 'api/pong/match', cors: ['*'] })
-export class MatchGateWay implements OnGatewayDisconnect {
+export class MatchGateWay implements OnGatewayDisconnect, OnGatewayConnection {
   constructor(
     private matchService: MatchService,
     private pongService: PongService,
@@ -36,15 +42,15 @@ export class MatchGateWay implements OnGatewayDisconnect {
   @SubscribeMessage('match')
   handleMessage(
     @MessageBody() message: MatchMessage,
-    @ConnectedSocket() client: Socket,
+    @ConnectedSocket() client: UserSocket,
   ) {
-    console.log(`connected: ${message.uid}`)
+    console.log(`connected: ${client.uid}`)
     let match: { left: MatchData; right: MatchData } | null = null
     if (message.matchType === 'quick') {
-      match = this.matchService.matchQuick({ uid: message.uid, socket: client })
+      match = this.matchService.matchQuick({ uid: client.uid, socket: client })
     } else {
       match = this.matchService.matchRanked({
-        uid: message.uid,
+        uid: client.uid,
         socket: client,
       })
     }
@@ -56,7 +62,7 @@ export class MatchGateWay implements OnGatewayDisconnect {
   @SubscribeMessage('movePaddle')
   handleMovePaddle(
     @MessageBody() message: PongKeyEvent,
-    @ConnectedSocket() client: Socket,
+    @ConnectedSocket() client: UserSocket,
   ) {
     const { game, isLeftSide } = this.pongService.getGameBySocketId(client.id)
 
@@ -67,11 +73,36 @@ export class MatchGateWay implements OnGatewayDisconnect {
     game.changePaddleVelocity(isLeftSide ? 'left' : 'right', direction)
   }
 
-  handleDisconnect(client: Socket) {
+  handleDisconnect(client: UserSocket) {
     this.matchService.removeFromQueue(client)
     const game = this.pongService.getGameBySocketId(client.id)
     if (game && game.game.getWinner() === null) {
       game.game.forceSetWinner(game.isLeftSide ? 'right' : 'left')
+    }
+  }
+
+  handleConnection(client: UserSocket) {
+    const authString = client.handshake.headers.authorization
+
+    if (!authString) {
+      client.disconnect()
+      return
+    }
+
+    const accessToken = authString.split(' ')[1]
+    if (!accessToken) {
+      client.disconnect()
+      return
+    }
+
+    try {
+      const decoded = jwt.verify(
+        accessToken,
+        jwtConstants.secret,
+      ) as jwt.JwtPayload
+      client.uid = Number(decoded.id)
+    } catch {
+      client.disconnect()
     }
   }
 }
