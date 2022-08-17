3737cd76a53aee0a2abc7f3fc35ed86aed133dd5
scarf <greenscarf005@gmail.com>
2022-08-17 21:22:00 +0900

    dotenv를 서브모듈로 추가, 기존 바인드 마운트 방식 재추가 (#123)
    * build: dotenv를 서브모듈로 추가
    
    기존 .env는 dotenv 레포지토리에서 수동으로 복사해 붙여넣어야 했음. 서브모듈을 쓰므로 업데이트가 있을 때마다 git pull만 하면 됨
    
    * docs: 서브모듈 설치법 수정
    
    * build: 전체 디렉터리를 바인드 마운트로 쓰는 방식 복구
    
    * feat: npm ci로 바꾸어 속도 향상

--- README.md
+++ README.md
@@ -2,7 +2,18 @@
 
 즐~~~~~거운 웹서비스
 
-`.env.sample` 참고해서 같은 위치에 `.env`파일 작성해주세요
+## 설치
+
+### 환경 변수
+
+- 환경 변수는 `transcendence/dotenv` 서브모듈에 있습니다
+
+```bash
+git config -g submodule.recurse true
+git submodule update --init --recursive
+```
+
+을 하여 서브모듈을 초기화하세요
 
 개발모드:
 `./dc.sh dev up --build`
@@ -10,9 +21,9 @@
 프로덕션 모드:
 `./dc.sh prod up --build`
 
-dc.sh에서 npm install이 되어있지 않을 때 자동으로 install하는 로직이 들어있지만, 잘 작동하지 않는 경우 install.sh를 실행해주세요
+dc.sh에서 npm ci이 되어있지 않을 때 자동으로 install하는 로직이 들어있지만, 잘 작동하지 않는 경우 install.sh를 실행해주세요
 
-## docs
+## 문서
 
 ### swagger
 
--- dc.sh
+++ dc.sh
@@ -1,28 +1,48 @@
 #! /bin/bash
 
+# Setup ARGV
+TYPE=$1
+shift
+ARGS=$@
+
+# Constants
 DOCKER_COMPOSE_DEV=$(dirname $0)/docker-compose.dev.yml
 DOCKER_COMPOSE_PROD=$(dirname $0)/docker-compose.prod.yml
+DOCKER_COMPOSE_BIND=$(dirname $0)/docker-compose.bind.yml
 
 BACKEND_ROOT=$(dirname $0)/backend/app
 FRONT_ROOT=$(dirname $0)/frontend/app
 
-function print_usage_and_exit() {
-	echo "Usage: $0 <dev|prod> <command> <...args>"
-	exit 1
+print_usage_and_exit() {
+  echo "Usage: $0 <dev|prod> <command> <...args>"
+  exit 1
 }
 
-test $# -lt 2 && print_usage_and_exit 
+setup_dependencies() {
+  test -d $BACKEND_ROOT/node_modules || (cd $BACKEND_ROOT && npm ci)
+  test -d $FRONT_ROOT/node_modules || (cd $FRONT_ROOT && npm ci)
+  npm ci
+}
 
-if [ $1 = "dev" ]; then	
-	TARGET_DOCKER_COMPOSE=$DOCKER_COMPOSE_DEV
-	test -d $BACKEND_ROOT/node_modules || (cd $BACKEND_ROOT && npm install)
-	test -d $FRONT_ROOT/node_modules || (cd $FRONT_ROOT && npm install)
-elif [ $1 = "prod" ]; then
-	TARGET_DOCKER_COMPOSE=$DOCKER_COMPOSE_PROD
-else
-	print_usage_and_exit
-fi
+run_compose() {
+  local file=$1
+  exec docker-compose -f $file $ARGS
+}
+
+case $TYPE in
+  dev)
+    setup_dependencies
+    run_compose $DOCKER_COMPOSE_DEV
+    ;;
+  bind)
+    run_compose $DOCKER_COMPOSE_BIND
+    ;;
+  prod)
+    run_compose $DOCKER_COMPOSE_PROD
+    ;;
+  *)
+    print_usage_and_exit
+    ;;
+esac
 
 shift
-
-exec docker-compose -f $TARGET_DOCKER_COMPOSE $@
