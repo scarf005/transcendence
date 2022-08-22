bac072e9fbfbd87f8266992965ed35ab08b785b3
cjeon <64446026+42-cjeon@users.noreply.github.com>
2022-08-22 17:14:53 +0900

    feat: 게임 옵션 선택창 개선 (#251)

--- frontend/app/src/view/GameOption.tsx
+++ frontend/app/src/view/GameOption.tsx
@@ -1,65 +1,106 @@
-import { useState } from 'react'
-import Radio from '@mui/material/Radio'
-import RadioGroup from '@mui/material/RadioGroup'
+import React, { useState } from 'react'
+import Checkbox from '@mui/material/Checkbox'
 import FormControlLabel from '@mui/material/FormControlLabel'
 import FormControl from '@mui/material/FormControl'
 import Button from '@mui/material/Button'
+import { ButtonGroup, Paper, Typography } from '@mui/material'
+type GameMode = 'classic' | 'sizedown' | 'speedup' | 'ranked'
+
+type GameInfo = {
+  mode: GameMode
+  isPrivate: boolean
+}
+
+const ClassicMode = () => {
+  return <Typography>1972년의 퐁을 그대로 가져왔습니다.</Typography>
+}
+
+const SizeDownMode = () => {
+  return <Typography>패들의 크기가 점점 작아집니다.</Typography>
+}
+
+const SpeedUpMode = () => {
+  return (
+    <Typography>공이 점점 빨라집니다! 언제까지 버틸 수 있을까요?</Typography>
+  )
+}
+
+const RankedMode = () => {
+  return <Typography>classic룰로 점수를 걸고 싸웁니다.</Typography>
+}
+
+const ModeExplain = (props: { mode: GameMode }) => {
+  switch (props.mode) {
+    case 'classic':
+      return <ClassicMode />
+    case 'sizedown':
+      return <SizeDownMode />
+    case 'speedup':
+      return <SpeedUpMode />
+    case 'ranked':
+      return <RankedMode />
+  }
+}
 
 export const PongMatchForm = (props: {
   requestMatch: (matchData: any) => void
 }) => {
-  const [gameMode, setGameMode] = useState('classic')
-  const submitRanked = () => {
-    props.requestMatch({
-      mode: 'ranked',
-      isPrivate: false,
+  const [gameInfo, setGameInfo] = useState<GameInfo>({
+    mode: 'classic',
+    isPrivate: false,
+  })
+
+  const changeGameMode = (mode: GameMode) => {
+    setGameInfo((value: GameInfo) => {
+      return {
+        mode: mode ? mode : value.mode,
+        isPrivate: value.isPrivate,
+      }
     })
   }
 
-  const submitQuick = () => {
-    props.requestMatch({
-      mode: gameMode,
-      isPrivate: false,
+  const changeIsPrivate = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setGameInfo({
+      mode: 'classic',
+      isPrivate: e.target.checked,
     })
   }
 
   return (
     <FormControl>
+      <FormControlLabel
+        control={<Checkbox onChange={changeIsPrivate} />}
+        label="친구와 함께하기"
+      />
+      <ButtonGroup>
+        <Button variant="contained" onClick={() => changeGameMode('classic')}>
+          Classic
+        </Button>
+        <Button variant="contained" onClick={() => changeGameMode('speedup')}>
+          Speed-Up
+        </Button>
+        <Button variant="contained" onClick={() => changeGameMode('sizedown')}>
+          Size-Down
+        </Button>
+        <Button
+          variant="contained"
+          disabled={gameInfo.isPrivate}
+          onClick={() => changeGameMode('ranked')}
+        >
+          Ranked
+        </Button>
+      </ButtonGroup>
+      <Paper>
+        <ModeExplain mode={gameInfo.mode} />
+      </Paper>
       <Button
-        onClick={submitRanked}
-        variant="outlined"
-        style={{ margin: '0.5rem' }}
-      >
-        Rank Game
-      </Button>
-      <Button
-        onClick={submitQuick}
-        variant="outlined"
-        style={{ margin: '0.5rem' }}
-      >
-        Quick Start
-      </Button>
-      <RadioGroup
-        row
-        aria-labelledby="demo-row-radio-buttons-group-label"
-        name="row-radio-buttons-group"
-        value={gameMode}
-        onChange={(e) => {
-          setGameMode(e.target.value)
+        variant="contained"
+        onClick={() => {
+          props.requestMatch(gameInfo)
         }}
       >
-        <FormControlLabel value="classic" control={<Radio />} label="Classic" />
-        <FormControlLabel
-          value="speedup"
-          control={<Radio />}
-          label="Speed-up"
-        />
-        <FormControlLabel
-          value="sizedown"
-          control={<Radio />}
-          label="Size-down"
-        />
-      </RadioGroup>
+        PLAY!
+      </Button>
     </FormControl>
   )
 }
